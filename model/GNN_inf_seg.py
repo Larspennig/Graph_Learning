bfc49257a3e2f55b41ca86b578e0ca9b847ad15f
Lars <lpennig@t-online.de>
2024-08-06 17:56:18 +0200

    asdf

--- model/GNN_inf_seg.py
+++ model/GNN_inf_seg.py
@@ -13,7 +13,7 @@ class Lightning_GNN(LightningModule):
         super().__init__()
         if config['model'] == 'standard':
             self.model = TransformerGNN(config=config)
-        #elif config['model'] == 'super':
+        # elif config['model'] == 'super':
         #    self.model = TransformerGNN_super(config=config)
         elif config['model'] == 'super_simple':
             self.model = TransformerGNN_super_simple(config=config)
@@ -38,7 +38,8 @@ class Lightning_GNN(LightningModule):
                  batch_size=self.config['batch_size'])
         self.log('batch_size', 2)
         values = output.max(dim=1).indices
-        accr = torch.sum(values == target.to(self.dev))/len(target.to(self.dev))
+        accr = torch.sum(values == target.to(self.dev)) / \
+            len(target.to(self.dev))
         self.log('train_acc', accr, on_epoch=True,
                  batch_size=self.config['batch_size'])
         return loss
@@ -51,7 +52,8 @@ class Lightning_GNN(LightningModule):
         self.log('val_loss', loss.item(), on_epoch=True,
                  batch_size=self.config['batch_size'])
         values = output.max(dim=1).indices
-        accr = torch.sum(values == target.to(self.dev))/len(target.to(self.dev))
+        accr = torch.sum(values == target.to(self.dev)) / \
+            len(target.to(self.dev))
         self.log('val_acc', accr, on_epoch=True,
                  batch_size=self.config['batch_size'])
         '''
@@ -84,10 +86,11 @@ class Lightning_GNN(LightningModule):
         accr = torch.sum(values == target)/len(target)
         self.log('test_acc', accr, on_epoch=True,
                  batch_size=self.config['batch_size'])
-        
+
         # Compute MIoU
         num_classes = self.config['num_classes']
-        confusion_matrix = torch.zeros(num_classes, num_classes, dtype=torch.int64, device=self.device)
+        confusion_matrix = torch.zeros(
+            num_classes, num_classes, dtype=torch.int64, device=self.device)
 
         for t, p in zip(target.view(-1), values.view(-1)):
             confusion_matrix[t.long(), p.long()] += 1
@@ -100,19 +103,23 @@ class Lightning_GNN(LightningModule):
             IoU[cls] = TP / (TP + FP + FN + 1e-10)  # Avoid division by zero
 
         MIoU = IoU[IoU.nonzero()].mean()
-        self.log('test_miou', MIoU, on_epoch=True, batch_size=self.config['batch_size'])
-        
+        self.log('test_miou', MIoU, on_epoch=True,
+                 batch_size=self.config['batch_size'])
+
         return accr
-    
+
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.config['learning_rate'], momentum=0.9, weight_decay=0.0001)
-        
+        optimizer = torch.optim.SGD(self.model.parameters(
+        ), lr=self.config['learning_rate'], momentum=0.9, weight_decay=0.0001)
+
         scheduler = {
-            'scheduler': StepLR(optimizer, gamma=0.3, step_size=30),
-            'interval': 'epoch', 
-            'frequency': 2
+            'scheduler': StepLR(optimizer, gamma=0.3, step_size=35),
+            'interval': 'epoch',
+            'frequency': 1
         }
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
+
+
 '''
     def configure_optimizers(self):
         return torch.optim.SGD(self.model.parameters(), lr=self.config['learning_rate'], momentum=0.9, weight_decay=0.0001)
