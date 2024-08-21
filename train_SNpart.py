afdecb30bef871700b50e55e9dd0e5ee92651084
Lars <lpennig@t-online.de>
2024-08-21 20:56:30 +0200

    sdfg

--- train_SNpart.py
+++ train_SNpart.py
@@ -56,8 +56,8 @@ def main():
     checkpoint_filename = "{epoch:02d}-{train_loss:.2f}"
 
     # copy model to output dir
-    os.makedirs(output_dir, exist_ok=True)
-    os.system('cp -r model/model_seg_gctx.py' + output_dir)
+    #os.makedirs(output_dir, exist_ok=True)
+    #os.system('cp -r model/model_seg_gctx.py' + output_dir)
 
     # Set up logger
     wandb_logger = WandbLogger(
